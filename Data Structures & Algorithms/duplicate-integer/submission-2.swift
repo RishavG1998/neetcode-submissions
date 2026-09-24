69c86e47cf1e3299f70cde858cbc051e0eed03c3
RishavG1998 <91869768+RishavG1998@users.noreply.github.com>
2026-09-24 16:00:58 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.swift
@@ -0,0 +1,26 @@
+class Solution {
+    func hasDuplicate(_ nums: [Int]) -> Bool {
+        // var numsHashMap: [Int: Int] = [:]
+
+        // for num in nums {
+        //     if numsHashMap[num] != nil {
+        //         return true
+        //     } else {
+        //         numsHashMap[num] = 1
+        //     }
+        // }
+        // return false
+
+        // var numsSet = Set<Int>()
+        // for num in nums {
+        //     if numsSet.contains(num) {
+        //         return true
+        //     } else {
+        //         numsSet.insert(num)
+        //     }
+        // }
+        // return false
+
+        return Set(nums).count < nums.count
+    }
+}
