252d09999981d8b5392a82be715f4b1acc7db2c5
RishavG1998 <91869768+RishavG1998@users.noreply.github.com>
2026-09-24 17:14:36 +0530

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.swift
@@ -0,0 +1,55 @@
+class Solution {
+    func groupAnagrams(_ strs: [String]) -> [[String]] {
+
+        // var sortedStrings = [String]()
+
+        // for str in strs {
+        //     sortedStrings.append(String(str.sorted()))
+        // }
+
+        // var result = [[String]]()
+        // var visited = Array(repeating: false, count: strs.count)
+
+        // for i in 0..<sortedStrings.count {
+        //     if visited[i] {
+        //         continue
+        //     }
+             
+        //     var cur = sortedStrings[i]
+        //     var grp = [strs[i]]
+        //     visited[i] = true
+
+        //     for j in i+1..<sortedStrings.count {
+        //         if sortedStrings[j] == cur {
+        //             grp.append(strs[j])
+        //             visited[j] = true
+        //         }
+        //     }
+        //     result.append(grp)
+
+        // }
+        // return result
+
+        
+        // var res = [String: [String]]()
+
+        // for str in strs {
+        //     let sortedStr = String(str.sorted())
+        //     res[sortedStr, default: []].append(str)
+        // }
+
+        // return Array(res.values)
+
+        var res = [Array<Int>: [String]]()
+
+        for str in strs {
+            var count = [Int](repeating: 0, count: 26)
+
+            for character in str {
+                count[Int(character.asciiValue!) - 97] += 1
+            }
+            res[count, default: []].append(str)
+        }
+        return Array(res.values)
+    }
+}
